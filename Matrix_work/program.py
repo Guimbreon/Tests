c594a1a3a9f44cd7bcf0ed884848d6424bdfebd6
Guimbreon <gui.acs03@gmail.com>
2023-02-16 23:33:01 +0000

    New tests in html

--- Matrix_work/program.py
+++ Matrix_work/program.py
@@ -8,25 +8,30 @@ im_matrix="""
 4 5 6
 7 8 9
 """
-#sum(matrix1,matrix2)
 class data():
-    def __init__(self,matrix):
-        
+    def lines(self,matrix):
+        all_lines=[]
         for lines in matrix:
-            self.line=lines
-        return self.line
-
-def make_matrix():
+            all_lines+=lines
+        return all_lines
+def make_matrix(number_of_matrix):
+    choice="1"
     """helps you make matrix's, here's how:
     You have two options:
         -1:A random matrix-> the program will generate for you a random matrix for you to work with,
         -2:A personalized matrix-> You give the program a specific matrix that it'll work with.
     """
-    lines=[]
-    choice=input("\nDo you want:\n1-A random matrix\n2-A personalized matrix\n>>")
+    print(f"{number_of_matrix}\n")
+
+    #choice=input("\nDo you want:\n1-A random matrix\n2-A personalized matrix\n>>")
+    
     if choice=="1":
+        """
         column_size=random.randint(1,5)
         line_size=random.randint(1,5)
+        """
+        column_size,line_size=8,4
+        n_m=[column_size,line_size]
         i,p=0,0
         line=[]
         matrix=[]
@@ -52,19 +57,94 @@ def make_matrix():
                 quit()
         column_size=n_m[0]
         line_size=n_m[1]
-    return matrix
+    return matrix, n_m
 #to sum matrixs
 def sum(matrix1,matrix2):
+    """Summ of the matrixs
+
+    Args:
+        matrix1 (list): The first matrix
+        matrix2 (list): The second matrix
+
+    Returns:
+        int: The matrix after the Summ operations!
+    """
     if len(matrix1)!=len(matrix2):
         print("The size(line x column) of the matrix's are not the same!")
         quit()
+    else:
+        i=0
+        new_matrix=[]
+        for numbers in matrix1:
+            new_matrix.append(numbers+matrix2[i])
+            i+=1
+        
+        i=0
+        matrix=[]
+        line=[]
+        for item in new_matrix:
+            line.append(item)
+            i+=1
+            if i==n_m[1]:
+                matrix.append(line)
+                line=[]
+                i=0
+        print("The sum of the matrixs is the following:")
+        for line in matrix:
+            print(line)
+    return matrix
+def sub(matrix1,matrix2):
+    """Substraction of matrixs
+
+    Args:
+        matrix1 (list): matrix number one
+        matrix2 (list): matrix numver two
+
+    Returns:
+        list: The new matrix after substractions are made!
+    """
+    if len(matrix1)!=len(matrix2):
+        print("The size(line x column) of the matrix's are not the same!")
+        quit()
+    else:
+        i=0
+        new_matrix=[]
+        for numbers in matrix1:
+            new_matrix.append(numbers-matrix2[i])
+            i+=1
+        
+        i=0
+        matrix=[]
+        line=[]
+        for item in new_matrix:
+            line.append(item)
+            i+=1
+            if i==n_m[1]:
+                matrix.append(line)
+                line=[]
+                i=0
+        print("The sum of the matrixs is the following:")
+        for line in matrix:
+            print(line)
+    return matrix
 #column_size,line_size=matrix_size()
 
-matrix=make_matrix()
-numbers=data(matrix)
-print(self.line)
-print(matrix)
+matrix1,n_m=make_matrix("This is the first matrix!")
+numbers1=data()
+lines1=numbers1.lines(matrix1)
+print(matrix1)
+matrix2,n_m=make_matrix("This is the second matrix!")
+numbers2=data()
+lines2=numbers2.lines(matrix2)
+print(matrix2)
+main_choice=input("What operation do you want to do?\n1-Sum\n2-Substration\n>>")
+if main_choice=="1":
+    sum_of_matrix=sum(lines1,lines2)
+elif main_choice=="2":
+    sub_of_matriz=sub(lines1,lines2)
+    """
 for line in matrix:
     print(line,type(matrix),type(line),(type(line[0])))
     for item in line:
         print(item)
+"""
